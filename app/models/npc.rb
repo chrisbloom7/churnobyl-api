401c6e0add05b6d568ef135b25d2a5fc6adcfd75
Chris Bloom <chrisbloom7@github.com>
2020-05-01 19:33:21 -0400

    add some persistance methods for npcs

--- app/models/npc.rb
+++ app/models/npc.rb
@@ -1,13 +1,35 @@
 class NPC < ApplicationRecord
-  def self.random
-    npc = self.new
-    npc.first_name = RandomTables::FirstName.random.value
-    npc.surname = RandomTables::Surname.hyphenated
-    npc.age = RandomTables::AgeGroup.random.value
-    npc.attitude = RandomTables::Attitude.random.value
-    npc.ancestry = RandomTables::Ancestry.mixed
-    npc.gender = RandomTables::GenderExpression.random.value
-    npc.origin = RandomTables::Origin.random.value
-    npc
+  class << self
+    def random
+      npc = self.new
+      npc.first_name = RandomTables::FirstName.random.value
+      npc.surname = RandomTables::Surname.hyphenated
+      npc.age = RandomTables::AgeGroup.random.value
+      npc.attitude = RandomTables::Attitude.random.value
+      npc.ancestry = RandomTables::Ancestry.mixed
+      npc.gender = RandomTables::GenderExpression.random.value
+      npc.origin = RandomTables::Origin.random.value
+      npc
+    end
+
+    def list(size)
+      npcs = []
+      size.times do
+        npcs << self.random
+      end
+      npcs
+    end
+
+    def create_random
+      npc = self.random
+      npc.save
+      npc
+    end
+
+    def create_list(size)
+      list = self.list(size)
+      list.each(&:save)
+      list
+    end
   end
 end
--- spec/models/npc_spec.rb
+++ spec/models/npc_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe NPC, type: :model do
   describe ".random" do
     let(:npc) { NPC.random }
 
-    it "generates a random NPC" do
-      expect(npc).to be_a(NPC)
+    it "generates an unpersisted random NPC" do
+      expect(npc).to be_a_new(NPC)
       expect(npc.age).to_not be_nil
       expect(npc.ancestry).to_not be_nil
       expect(npc.attitude).to_not be_nil
@@ -13,6 +13,38 @@ RSpec.describe NPC, type: :model do
       expect(npc.gender).to_not be_nil
       expect(npc.origin).to_not be_nil
       expect(npc.surname).to_not be_nil
+    end
+  end
+
+  describe ".list" do
+    let(:size) { 3 }
+    let(:list) { NPC.list(size) }
+
+    it "generates a list of unpersisted NPCs" do
+      expect(list).to be_an(Array)
+      expect(list.size).to eq(size)
+      expect(list).to all(be_a_new(NPC))
+    end
+  end
+
+  describe ".create_random" do
+    let(:npc) { NPC.create_random }
+
+    it "persists a random NPC to the database" do
+      expect(npc).to be_a(NPC)
+      expect(npc).to be_persisted
+    end
+  end
+
+  describe ".create_list" do
+    let(:size) { 3 }
+    let(:list) { NPC.create_list(size) }
+
+    it "persists a list of random NPCs to the database" do
+      expect(list).to be_an(Array)
+      expect(list.size).to eq(size)
+      expect(list).to all(be_a(NPC))
+      expect(list).to all(be_persisted)
     end
   end
 end

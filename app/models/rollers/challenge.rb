d3f502f62029f160774cf7c67c774144adbd2880
Chris Bloom <chrisbloom7@github.com>
2020-10-18 10:27:12 -0400

    Add roller class for AGE challenge rolls

---
+++ app/models/rollers/challenge.rb
@@ -0,0 +1,27 @@
+module Rollers
+  module Challenge
+    extend self
+
+    def drama
+      [[d6, d6], d6]
+    end
+
+    def roll
+      drama.flatten.sort
+    end
+
+    def simple
+      roll.sum
+    end
+
+    def drop
+      (roll + [d6]).sort[1..3]
+    end
+
+    private
+
+    def d6
+      rand(6) + 1
+    end
+  end
+end

8df3e08eba46af076365d1ac58297b73b6f8fee0
Chris Bloom <chrisbloom7@github.com>
2020-10-18 18:14:09 -0400

    Add statted npc seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-unless Collection.find_by(name: "NPC")
+puts "Generating seed data"
+
+unless Collection.named("npc").present?
+  puts "Creating npc collection"
+
   templates = []
   templates << Template.new(label: "First Name", generator: "FirstName")
   templates << Template.new(label: "Surname",    generator: "HyphenatedSurname")
@@ -16,6 +20,40 @@ unless Collection.find_by(name: "NPC")
   templates << Template.new(label: "Gender",     generator: "GenderExpression")
   templates << Template.new(label: "Origin",     generator: "Origin")
   Collection.create!(name: "npc",
-                                  default_label: "NPC",
-                                  templates: templates)
+                     default_label: "NPC",
+                     templates: templates)
+else
+  puts "npc collection already exists"
 end
+
+unless Collection.named("statted_npc").present?
+  puts "Creating statted_npc collection"
+
+  templates = []
+  templates << Template.new(label: "First Name", generator: "FirstName")
+  templates << Template.new(label: "Surname",    generator: "HyphenatedSurname")
+  templates << Template.new(label: "Age",        generator: "AgeGroup")
+  templates << Template.new(label: "Attitude",   generator: "Attitude")
+  templates << Template.new(label: "Ancestry",   generator: "MixedAncestry")
+  templates << Template.new(label: "Gender",     generator: "GenderExpression")
+  templates << Template.new(label: "Origin",     generator: "Origin")
+
+  templates << Template.new(label: "Accuracy",      generator: "AbilityScore")
+  templates << Template.new(label: "Communication", generator: "AbilityScore")
+  templates << Template.new(label: "Constitution",  generator: "AbilityScore")
+  templates << Template.new(label: "Dexterity",     generator: "AbilityScore")
+  templates << Template.new(label: "Fighting",      generator: "AbilityScore")
+  templates << Template.new(label: "Intelligence",  generator: "AbilityScore")
+  templates << Template.new(label: "Perception",    generator: "AbilityScore")
+  templates << Template.new(label: "Strength",      generator: "AbilityScore")
+  templates << Template.new(label: "Willpower",     generator: "AbilityScore")
+
+  Collection.create!(name: "statted_npc",
+                     default_label: "Statted NPC",
+                     templates: templates)
+
+else
+  puts "statted_npc collection already exists"
+end
+
+puts "Seeding complete"
